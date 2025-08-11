eab4bc31103e2ccbe7a8af8c8869f8acfbf87ec6
Vanessa Chitarra Ussalim <vanvonholly@Vanessas-MacBook-Pro.local>
2025-08-11 16:54:49 +0700

    new eatout page :D

---
+++ screens/EatOut/WriteReview.tsx
@@ -0,0 +1,21 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { ScrollView } from 'react-native-gesture-handler';
+
+export default function WriteReview() {
+    return (
+        <ScrollView>
+
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+
+}
+);
